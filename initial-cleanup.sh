1b47942af06a4c05b8fc9099c0f036fd28eecfbb
Cris P <105011867+CriBP@users.noreply.github.com>
2025-01-12 23:43:36 -0800

    Add files via upload

--- initial-cleanup.sh
+++ initial-cleanup.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-###############################################################################
-# This script cleans-up Ubuntu installation
-# Used a lot of research to make the installation lighter and safer
-###############################################################################
-
-# Install language support
+set -v
+#########################################################################
+# This script cleans-up Ubuntu installation                             #
+# Used a lot of research to make the installation lighter and safer     #
+# Press CTRL-Z to stop the script                                       #
+#########################################################################
+#
+# ██╗   ██╗██████╗ ██╗   ██╗███╗   ██╗████████╗██╗   ██╗     ██████╗██╗     ███████╗ █████╗ ███╗   ██╗
+# ██║   ██║██╔══██╗██║   ██║████╗  ██║╚══██╔══╝██║   ██║    ██╔════╝██║     ██╔════╝██╔══██╗████╗  ██║
+# ██║   ██║██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║   ██║    ██║     ██║     █████╗  ███████║██╔██╗ ██║
+# ██║   ██║██╔══██╗██║   ██║██║╚██╗██║   ██║   ██║   ██║    ██║     ██║     ██╔══╝  ██╔══██║██║╚██╗██║
+# ╚██████╔╝██████╔╝╚██████╔╝██║ ╚████║   ██║   ╚██████╔╝    ╚██████╗███████╗███████╗██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝   ╚═╝    ╚═════╝      ╚═════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+#
+echo -e "\033[96m Install language support\033[0m"
 sudo apt install $(check-language-support)
 
-# Remove SWAP File on PC's with 4GB+ RAM
+echo -e "\033[96m Remove SWAP File on PC's with 4GB+ RAM\033[0m"
 sudo swapoff /swap.img
 sudo rm /swap.img
 sudo swapoff -a
 
-# Then comment out or delete the following line in /etc/fstab:
-# /swap.img      none    swap    sw      0       0
+echo -e "\033[96m Then comment out or delete the following line in /etc/fstab:\033[0m"
+echo -e "\033[96m /swap.img      none    swap    sw      0       0 \033[0m"
 
-# Change to black backgound:
+echo -e "\033[96m Change to black backgound:\033[0m"
 gsettings get org.gnome.desktop.background picture-uri
 gsettings set org.gnome.desktop.background picture-uri ""
 gsettings get org.gnome.desktop.background picture-uri-dark
@@ -25,7 +34,7 @@ gsettings set org.gnome.desktop.background primary-color '#000000'
 gsettings get org.gnome.desktop.background show-desktop-icons
 gsettings set org.gnome.desktop.background show-desktop-icons true
 
-# Disable Event Sounds
+echo -e "\033[96m Disable Event Sounds\033[0m"
 sudo rmmod pcspkr ; echo "blacklist pcspkr" >>/etc/modprobe.d/blacklist.conf
 gsettings get org.gnome.desktop.sound allow-volume-above-100-percent
 gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
@@ -38,25 +47,29 @@ gsettings set org.gnome.desktop.sound event-sounds false
 gsettings get com.ubuntu.sound allow-amplified-volume
 gsettings set com.ubuntu.sound allow-amplified-volume true
 
-# Add welcome message (comment to disable)
+echo -e "\033[96m Add welcome message (comment to disable)\033[0m"
 gsettings get org.gnome.login-screen banner-message-enable
 gsettings set org.gnome.login-screen banner-message-enable true
 gsettings get org.gnome.login-screen banner-message-text
 gsettings set org.gnome.login-screen banner-message-text "Welcome to Ubuntu"
 
-# Privacy Settings: gsettings list-keys org.gnome.desktop.privacy 
+echo -e "\033[96m Privacy Settings: gsettings list-keys org.gnome.desktop.privacy\033[0m"
 gsettings get org.gnome.desktop.privacy disable-camera
-# gsettings set org.gnome.desktop.privacy disable-camera true
+
+echo -e "\033[96m gsettings set org.gnome.desktop.privacy disable-camera true\033[0m"
 gsettings get org.gnome.desktop.privacy disable-microphone
-# gsettings set org.gnome.desktop.privacy disable-microphone true
+
+echo -e "\033[96m gsettings set org.gnome.desktop.privacy disable-microphone true\033[0m"
 gsettings get org.gnome.desktop.privacy disable-sound-output
-# gsettings set org.gnome.desktop.privacy disable-sound-output true
+
+echo -e "\033[96m gsettings set org.gnome.desktop.privacy disable-sound-output true\033[0m"
 gsettings get org.gnome.desktop.privacy hide-identity
 gsettings set org.gnome.desktop.privacy hide-identity true
 gsettings get org.gnome.desktop.privacy remember-app-usage
 gsettings set org.gnome.desktop.privacy remember-app-usage false
 gsettings get org.gnome.desktop.privacy remember-recent-files
-# gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+echo -e "\033[96m gsettings set org.gnome.desktop.privacy remember-recent-files false\033[0m"
 gsettings get org.gnome.desktop.privacy remove-old-temp-files
 gsettings set org.gnome.desktop.privacy remove-old-temp-files true
 gsettings get org.gnome.desktop.privacy remove-old-trash-files
@@ -70,10 +83,10 @@ gsettings set org.gnome.desktop.privacy show-full-name-in-top-bar true
 gsettings get org.gnome.desktop.privacy usb-protection
 gsettings set org.gnome.desktop.privacy usb-protection true
 
-# Disable Rastersoft Ding
+echo -e "\033[96m Disable Rastersoft Ding\033[0m"
 gnome-extensions disable ding@rastersoft.com
 
-# Remove apt / apt-get files
+echo -e "\033[96m Remove apt / apt-get files\033[0m"
 sudo apt clean
 sudo apt -s clean
 sudo apt clean all
@@ -82,7 +95,8 @@ sudo apt-get clean
 sudo apt-get -s clean
 sudo apt-get clean all
 sudo apt-get autoclean
-# Update Ubuntu: 
+
+echo -e "\033[96m Update Ubuntu:\033[0m"
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
@@ -93,39 +107,48 @@ sudo apt-get clean
 sudo apt-get check
 sudo apt-get -f install
 sudo apt autoremove
-# Forced upgrade: 
+
+echo -e "\033[96m Forced upgrade:\033[0m"
 sudo do-release-upgrade -d
 sudo do-release-upgrade -m desktop
-# Get current kernel version - Start an LXTerminal session enter:
+
+echo -e "\033[96m Get current kernel version - Start an LXTerminal session enter:\033[0m"
 uname -r
-# View installed kernels:
+
+echo -e "\033[96m View installed kernels:\033[0m"
 dpkg -l | grep linux-image
-# Remove all unused kernels:
+
+echo -e "\033[96m Remove all unused kernels:\033[0m"
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
 sudo apt purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d') -y
 sudo dpkg --configure -a
 sudo update-grub
 
-# Remove orphaned libraries:
+echo -e "\033[96m Remove orphaned libraries:\033[0m"
 sudo apt-get remove --purge `deborphan`; sudo apt-get autoremove
 sudo deborphan | xargs sudo apt-get -y remove --purge
-# Cleaning unused configurations: 
+
+echo -e "\033[96m Cleaning unused configurations:\033[0m"
 sudo dpkg --purge `dpkg -l | egrep "^rc" | cut -d ' ' -f3`
 sudo apt-get purge $(dpkg -l | awk '/^rc/ { print $2 }')
-# Clean Thumbnail Cache: 
+
+echo -e "\033[96m Clean Thumbnail Cache:\033[0m"
 sudo rm -rf ~/.cache/thumbnails/*
-# Clean Apt Cache: 
+
+echo -e "\033[96m Clean Apt Cache:\033[0m"
 sudo du -sh /var/cache/apt
-#Remove Old Log Files
+
+echo -e "\033[96m Remove Old Log Files\033[0m"
 sudo rm -f /var/log/*gz
 sudo rm -rf /var/log/*.gz
 sudo rm -rf /var/log/*.1
-# Clean temporary files
+
+echo -e "\033[96m Clean temporary files\033[0m"
 sudo rm -rf /tmp/*
 sudo rm -rf /var/tmp/*
 rm -rf ~/.local/share/Trash/* /tmp/*
 
-# How to Remove Snap From Ubuntu (Snap seems to be an universal app-store pushed by Canonical without user acknowledge)
+echo -e "\033[96m How to Remove Snap From Ubuntu (Snap seems to be an universal app-store pushed by Canonical without user acknowledge)\033[0m"
 snap list
 sudo snap remove --purge package-name
 sudo rm -rf /var/cache/snapd/
@@ -133,58 +156,60 @@ sudo apt autoremove --purge snapd gnome-software-plugin-snap
 rm -fr ~/snap
 sudo apt-mark hold snapd
 
-# Install Firefox .deb package for Debian-based distributions (Recommended)
-# To install the .deb package through the APT repository, do the following:
-# Create a directory to store APT repository keys if it doesn't exist:
+echo -e "\033[96m Install Firefox .deb package for Debian-based distributions (Recommended)\033[0m"
+echo -e "\033[96m To install the .deb package through the APT repository, do the following:\033[0m"
+echo -e "\033[96m Create a directory to store APT repository keys if it doesn't exist:\033[0m"
 sudo install -d -m 0755 /etc/apt/keyrings
-# Import the Mozilla APT repository signing key:
+echo -e "\033[96m Import the Mozilla APT repository signing key:\033[0m"
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-# If you do not have wget installed, you can install it with: sudo apt-get install wget
-# The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. You may check it with the following command:
+echo -e "\033[96m If you do not have wget installed, you can install it with: sudo apt-get install wget\033[0m"
+echo -e "\033[96m The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. You may check it with the following command:\033[0m"
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-# Next, add the Mozilla APT repository to your sources list:
+echo -e "\033[96m Next, add the Mozilla APT repository to your sources list:\033[0m"
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-# Configure APT to prioritize packages from the Mozilla repository:
+echo -e "\033[96m Configure APT to prioritize packages from the Mozilla repository:\033[0m"
 echo '
 Package: *
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla
-# Update your package list, and install the Firefox .deb package:
+echo -e "\033[96m Update your package list, and install the Firefox .deb package:\033[0m"
 sudo apt-get update && sudo apt-get install firefox
-# Data migration: If you were using Snap or Flatpak before, you are required to import your profile:
-# Copy the existing files on your computer. Make sure that all copies of Firefox on your computer are completely closed before doing this:
-# Flatpak:
+echo -e "\033[96m Data migration: If you were using Snap or Flatpak before, you are required to import your profile:\033[0m"
+echo -e "\033[96m Copy the existing files on your computer. Make sure that all copies of Firefox on your computer are completely closed before doing this:\033[0m"
+echo -e "\033[96m Flatpak:\033[0m"
 mkdir -p ~/.mozilla/firefox/ && cp -a ~/.var/app/org.mozilla.firefox/.mozilla/firefox/* ~/.mozilla/firefox/
-# Snap:
+echo -e "\033[96m Snap:\033[0m"
 mkdir -p ~/.mozilla/firefox/ && cp -a ~/snap/firefox/common/.mozilla/firefox/* ~/.mozilla/firefox/ 
-# launch Firefox from the terminal with the command firefox -P. Select your desired profile. After this initial setup, the -P command will no longer be necessary.
+echo -e "\033[96m launch Firefox from the terminal with the command firefox -P. Select your desired profile. After this initial setup, the -P command will no longer be necessary.\033[0m"
 firefox -P
 
-# To list your partition sizes run: 
+echo -e "\033[96m To list your partition sizes run:\033[0m"
 df -Th | sort
-# Check the size of the main folders
+
+echo -e "\033[96m Check the size of the main folders\033[0m"
 sudo du -sxm  /[^p]* | sort -nr   | head -n 15
 
-# list all applications installed in the system: 
+echo -e "\033[96m list all applications installed in the system:\033[0m"
 for app in /usr/share/applications/*.desktop ~/.local/share/applications/*.desktop; do app="${app##/*/}"; echo "${app::-8}"; done
 
-# Display the size of the /var
+echo -e "\033[96m Display the size of the /var\033[0m"
 sudo du -sxm /var/* | sort -nr | head -n 15
 
-# Large size file May be useful to check if exist large size files everyware e for any users. The command for 750MB size files may be like:
+echo -e "\033[96m Large size file May be useful to check if exist large size files everyware e for any users. The command for 750MB size files may be like:\033[0m"
 sudo find / -size +750M -exec ls -lhG {} \; | more
 
-# How to remove MiniDLNA
+echo -e "\033[96m How to remove MiniDLNA\033[0m"
 sudo apt-get remove --auto-remove minidlna
 
-# Cleanup Services
+echo -e "\033[96m Cleanup Services\033[0m"
 sudo apt-get install sysv-rc-conf
 sudo sysv-rc-conf
 
-# Check RAM memory
+echo -e "\033[96m Check RAM memory\033[0m"
 free -m
 vmstat
-# Free RAM
-echo 3 > /proc/sys/vm/drop_caches
+
+echo -e "\033[96m Free RAM\033[0m"
+sudo echo 3 > /proc/sys/vm/drop_caches
 echo 3 | sudo tee /proc/sys/vm/drop_caches
